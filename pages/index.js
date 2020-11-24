5d24762ed5b78ef1000927ead9d69a1168aa10c9
Janko <mailodjanka@gmail.com>
2020-11-24 12:22:34 +0100

    Adds a Link to /auth from next/link. It's cool,works right out of the box

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React from "react";
+
+import Link from "next/link";
 
 const indexPage = () => {
   return (
     <div>
-      <h1>The Index Page</h1>
+      <h1>The Main Page</h1>
+      <p>
+        Go to:{" "}
+        <Link href="/auth">
+          <a>Auth</a>
+        </Link>{" "}
+      </p>
     </div>
   );
-}
+};
 
 export default indexPage;
