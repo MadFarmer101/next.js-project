6c0bd3691dcbc9d5577b6cc81d9ae9d2efdde80a
Janko <mailodjanka@gmail.com>
2020-11-25 12:38:48 +0100

    Changes IndexPage into class based component

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,23 @@
-import React from "react";
+import React, { Component } from "react";
 import Link from "next/link";
 import Router from "next/router";
 
-const indexPage = () => {
-  return (
-    <div>
-      <h1>The Main Page</h1>
-      <p>
-        Go to:{" "}
-        <Link href="/auth">
-          <a>Auth</a>
-        </Link>{" "}
-      </p>
-      <button onClick={() => Router.push("/auth")}>Go to Auth</button>
-    </div>
-  );
-};
 
-export default indexPage;
+class IndexPage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>The Main Page</h1>
+        <p>
+          Go to:{" "}
+          <Link href="/auth">
+            <a>Auth</a>
+          </Link>{" "}
+        </p>
+        <button onClick={() => Router.push("/auth")}>Go to Auth</button>
+      </div>
+    );
+  }
+}
+
+export default IndexPage;
