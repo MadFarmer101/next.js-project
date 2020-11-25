260e98eb9e123bd6f7115197396a30414b0ca0c5
Janko <mailodjanka@gmail.com>
2020-11-25 12:46:46 +0100

    Adds a appName in getInitialProps lifecycle method and calls on it as props in comp

--- pages/index.js
+++ pages/index.js
@@ -5,15 +5,14 @@ import Router from "next/router";
 
 class IndexPage extends Component {
 
-  static async getInitialProps(context) {
-    console.log(context)
-    return {}
+  static async getInitialProps() {
+    return {appName: "Super App"}
   }
 
   render() {
     return (
       <div>
-        <h1>The Main Page</h1>
+        <h1>The Main Page of {this.props.appName}</h1>
         <p>
           Go to:{" "}
           <Link href="/auth">
